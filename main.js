3da49146ebb8cfda283ab3ab6e66a330ad470e37
Eric Wooley <5wooley4@gmail.com>
2013-12-04 20:42:45 -0800

    missed a semicolon

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ var events = {
     });
     circle.on('click', function(){
       console.log("you clicked the circle!");
-    })
+    });
     // This adds a node to circle for the engine to work with.
     console.log()
     var state = {
